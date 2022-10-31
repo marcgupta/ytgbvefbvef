eeffd0c49279726ddded72086f16e9d0815ceae6
marcgupta <81474423+marcgupta@users.noreply.github.com>
2022-10-31 16:29:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML = '<img id="image_captured" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5 version:",ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Qorn6llzx/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded Successfully!");
+}
+
+function speak() {
+
+    var synth = window.speechSynthesis;
+    speak_data = "The prediction is" + results[0].label;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}
+
+function check() {
+
+    img = document.getElementById("image_captured");
+    
+    classifier.classify(img,gotResult);
+    
+    }
+
+    function gotResult(error,results) {
+
+        if (error) {
+        
+        console.error(error);
+        
+        }
+        else {
+        
+        console.log(results);
+        
+        document.getElementById("prediction").innerHTML = results[0].label;        
+        
+        speak();
+       
+        }
+        
+        }
